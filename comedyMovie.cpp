8a7ff2826c19c5b7f1344c3a6e41f432fe9430c2
parthgu <gparth02@hotmail.com>
2023-03-13 22:39:26 -0700

    implemented comedyMovie

--- comedyMovie.cpp
+++ comedyMovie.cpp
@@ -8,10 +8,20 @@ ComedyMovie::ComedyMovie()
 
 ComedyMovie::ComedyMovie(int stock, string director, string title, int year)
 {
+  this->type = 'F';
+  this->mediaType = 'D';
+  this->stock = stock;
+  this->director = director;
+  this->title = title;
+  this->year = year;
 }
 
-void ComedyMovie::display()
+void ComedyMovie::display(int spaces)
 {
+  for (int i = 0; i < spaces; i++)
+  {
+    cout << " ";
+  }
   cout << this->type;
   cout << "      ";
   cout << this->mediaType;
