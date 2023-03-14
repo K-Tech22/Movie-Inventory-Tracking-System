3c84b495862853a268aab2914da8d91d6b59fe1a
parthgu <gparth02@hotmail.com>
2023-03-13 22:40:06 -0700

    implemented dramaMovie

--- dramaMovie.cpp
+++ dramaMovie.cpp
@@ -8,10 +8,20 @@ DramaMovie::DramaMovie()
 
 DramaMovie::DramaMovie(int stock, string director, string title, int year)
 {
+  this->type = 'D';
+  this->mediaType = 'D';
+  this->stock = stock;
+  this->director = director;
+  this->title = title;
+  this->year = year;
 }
 
-void DramaMovie::display()
+void DramaMovie::display(int spaces)
 {
+  for (int i = 0; i < spaces; i++)
+  {
+    cout << " ";
+  }
   cout << this->type;
   cout << "      ";
   cout << this->mediaType;
