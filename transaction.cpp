f9d6315c4fc211a54b59526ebd79ef871cbf6063
parthgu <gparth02@hotmail.com>
2023-03-15 18:35:26 -0700

    comments for transaction

--- transaction.cpp
+++ transaction.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "transaction.h"
 
+/**
+ * Constructor
+ *
+ * @pre none
+ * @post Transaction object made
+ */
 Transaction::Transaction()
 {
-  // instance variables not set
 }
 
+/**
+ * Constructor
+ *
+ * @pre none
+ * @post instance variables will be set
+ */
 Transaction::Transaction(char mediaType, char transactionType, Movie mv)
 {
   this->mediaType = mediaType;
@@ -13,6 +24,12 @@ Transaction::Transaction(char mediaType, char transactionType, Movie mv)
   this->mv = mv;
 }
 
+/**
+ * display
+ *
+ * @pre none
+ * @post Details of transaction will be displayed to console
+ */
 void Transaction::display() const
 {
 
@@ -29,36 +46,72 @@ void Transaction::display() const
   }
 }
 
+/**
+ * getMediaType
+ *
+ * mediaType getter (char)
+ */
 char Transaction::getMediaType() const
 {
   return this->mediaType;
 }
 
+/**
+ * setMediaType
+ *
+ * mediaType setter
+ */
 void Transaction::setMediaType(char mType)
 {
   this->mediaType = mType;
 }
 
+/**
+ * getTransactionType
+ *
+ * transactionType getter (char)
+ */
 char Transaction::getTransactionType() const
 {
   return this->transactionType;
 }
 
+/**
+ * setTransactionType
+ *
+ * transactionType setter
+ */
 void Transaction::setTransactionType(char tType)
 {
   this->transactionType = tType;
 }
 
+/**
+ * getMovie
+ *
+ * Returns movie tied to transaction
+ */
 Movie Transaction::getMovie() const
 {
   return this->mv;
 }
 
+/**
+ * setMovie
+ *
+ * Sets movie tied to transaction
+ */
 void Transaction::setMovie(Movie mv)
 {
   this->mv = mv;
 }
 
+/**
+ * operator<<
+ *
+ * @pre none
+ * @post outputs transaction to console
+ */
 ostream &operator<<(ostream &out, const Transaction &t)
 {
   t.display();
--- transaction.h
+++ transaction.h
@@ -8,29 +8,85 @@ using namespace std;
 
 class Transaction
 {
+    /**
+     * operator<<
+     *
+     * @pre none
+     * @post outputs transaction to console
+     */
     friend ostream &operator<<(ostream &, const Transaction &);
 
 public:
+    /**
+     * Constructor
+     *
+     * @pre none
+     * @post Transaction object made
+     */
     Transaction();
+
+    /**
+     * Constructor
+     *
+     * @pre none
+     * @post instance variables will be set
+     */
     Transaction(char mediaType, char transactionType, Movie mv);
 
+    /**
+     * display
+     *
+     * @pre none
+     * @post Details of transaction will be displayed to console
+     */
     void display() const;
 
+    /**
+     * getMediaType
+     *
+     * mediaType getter (char)
+     */
     char getMediaType() const;
+
+    /**
+     * setMediaType
+     *
+     * mediaType setter
+     */
     void setMediaType(char mType);
 
+    /**
+     * getTransactionType
+     *
+     * transactionType getter (char)
+     */
     char getTransactionType() const;
+
+    /**
+     * setTransactionType
+     *
+     * transactionType setter
+     */
     void setTransactionType(char tType);
 
+    /**
+     * getMovie
+     *
+     * Returns movie tied to transaction
+     */
     Movie getMovie() const;
+
+    /**
+     * setMovie
+     *
+     * Sets movie tied to transaction
+     */
     void setMovie(Movie mv);
 
 private:
     char mediaType;
     char transactionType;
     Movie mv;
-    bool isError;
-    int errorCode;
 };
 
 #endif
